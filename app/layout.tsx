4ddd70379d02dbcf00e501141a5052e093dad294
Dereje Seifu <derejeseifu1991@gmail.com>
2025-06-03 21:58:51 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AI Visibility Checker',
+  description: 'AI Visibility hecker'
 }
 
 export default function RootLayout({
